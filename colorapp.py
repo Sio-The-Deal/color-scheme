31b08f1515200e58ff10203d7ec41d6126434541
changs3 <sio.chang3@mail.dcu.ie>
2023-05-11 23:54:43 +0100

    setup prompt command input

--- colorapp.py
+++ colorapp.py
@@ -14,3 +14,22 @@ app = Flask(__name__,
     static_url_path='', 
     static_folder='static'
 )
+
+def get_colors(msg):
+    prompt = f"""
+    You are a color scheme generating assistant that responds to text prompts for color schemes
+    Your should generate color schemes that fit the theme, mood, or instructions in the prompt.
+    The schemes should be between 2 and 5 colors.
+
+    Q: Convert the following verbal description of a color scheme into a list of colors: Google
+    A: ["#4285F4", "#DB4437", "#F4B400", "#F4B400", "#0F9D58"]
+
+    Q: Convert the following verbal description of a color scheme into a list of colors: christmas, warm, joy
+    A: ["#165B33", "#146B3A", "#EA4630", "#F8B229"]
+
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color scheme into a list of colors: {msg} 
+    A:
+    """
